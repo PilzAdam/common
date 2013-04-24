093dc4bca88056079138630db70283bab327d8a5
PilzAdam <pilzadam@minetest.net>
2013-04-24 17:19:22 +0200

    Add place and dug sounds to wool

--- mods/wool/init.lua
+++ mods/wool/init.lua
@@ -35,6 +35,7 @@ for _, row in ipairs(wool.dyes) do
 		description = desc.." Wool",
 		tiles = {"wool_"..name..".png"},
 		groups = {snappy=2,choppy=2,oddly_breakable_by_hand=3,flammable=3,wool=1},
+		sounds = default.node_sound_defaults(),
 	})
 	if craft_color_group then
 		-- Crafting from dye and white wool
